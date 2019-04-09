9b46dc026188fee4a67fa5ea4dacc696567983d2
hesioz <thelliercyril@gmail.com>
2019-04-09 09:57:52 +0200

    Création de construct()

---
+++ moduletest.php
@@ -0,0 +1,31 @@
+<?php
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class ModuleTest extends Module{
+    public function __construct()
+    {
+        $this->name = 'moduletest';
+        $this->tab = 'front_office_features';
+        $this->version = '1.0.0';
+        $this->author = 'Cyril Thellier';
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = [
+            'min' => '1.6',
+            'max' => _PS_VERSION_
+        ];
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Module pour le Test');
+        $this->description = $this->l('Le module a faire pour la candidature de stage chez prestarocket');
+
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+
+        if (!Configuration::get('MYMODULE_NAME')) {
+            $this->warning = $this->l('No name provided');
+        }
+    }
+}
